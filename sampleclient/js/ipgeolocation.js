a5dcf5e3339d91d01611d30d9f79abd32e95a032
unknown <olga1gordeyeva@gmail.com>
2019-05-29 00:08:35 -0400

    some style for map, user information added, marker added

--- sampleclient/js/ipgeolocation.js
+++ sampleclient/js/ipgeolocation.js
@@ -21,13 +21,31 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     // const map = document.getElementById("map");
     let map = initMap("map");
+    let markers = [];
+
     const elLocation = document.getElementById("ipgeolocation");
     const data = await fetch(ipgeolocation.list);
     const dataJson = await data.json();
+    let userMarker = new mapboxgl.Marker();
 
     dataJson.forEach(element => {
+
+        let userMarker = new mapboxgl.Marker()
+            .setLngLat([element.longitude, element.latitude])
+            .setPopup(
+                new mapboxgl.Popup({ className: 'here' }).setHTML(
+                    '<h1>You are here</h1>'
+                ))
+            .addTo(map)
+            .togglePopup();
+
         const tEl = document.createElement("div");
-        tEl.innerHTML = element.ip + " , " + element.country_name + " , " + element.country_capital + " , " + element.state_prov + " , " + element.district + " , " + element.city + " , " + element.country_flag + ' , ' + element.latitude + ' , ' + element.longitude;
+        tEl.classList.add("record");
+        tEl.innerHTML = element.ip + " , " + element.country_name + " , " + element.country_capital + " , " + element.state_prov
+            + " , " + element.district + " , " + element.city + " , " + '<img class="flag" src="' + element.country_flag + '" />';
+
+        // element.latitude + ' , ' + element.longitude
+
         elLocation.appendChild(tEl);
 
     });
