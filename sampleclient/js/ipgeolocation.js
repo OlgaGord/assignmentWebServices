96a425cf85a089c813ff8709e6a2f706beffdf32
unknown <olga1gordeyeva@gmail.com>
2019-05-31 07:56:49 -0400

    weather added to pop-up"

--- sampleclient/js/ipgeolocation.js
+++ sampleclient/js/ipgeolocation.js
@@ -17,8 +17,13 @@ const initMap = container => {
     return map;
 };
 
-
 document.addEventListener("DOMContentLoaded", async () => {
+    const weather = async (element) => {
+        var weatherUri = `/api/ipgeolocation/weather?lat=${element.longitude}&lon=${element.latitude}`;
+        const weather = await fetch(weatherUri);
+        const weatherJson = await weather.json();
+        return weatherJson;
+    }
     let map = initMap("map");
     let markers = [];
 
@@ -26,44 +31,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await fetch(ipgeolocation.list);
     const dataJson = await data.json();
     let userMarker = new mapboxgl.Marker();
-
     dataJson.forEach(element => {
+        weather(element).then(weatherJson => {
+            let userMarker = new mapboxgl.Marker()
+                .setLngLat([element.longitude, element.latitude])
+                .setPopup(
+                    new mapboxgl.Popup({ className: 'here' }).setHTML(
+                        '<h4>You visited our site  </h4>' + element.visits + "times" + '<img src="' + weatherJson.current.condition.icon + '"/>'
+                    ))
+                .addTo(map)
+                .togglePopup();
 
-
-        let userMarker = new mapboxgl.Marker()
-            .setLngLat([element.longitude, element.latitude])
-            .setPopup(
-                new mapboxgl.Popup({ className: 'here' }).setHTML(
-                    '<h4>You visited our site</h4>' + element.visits + "times"
-                ))
-            .addTo(map)
-            .togglePopup();
-
+        });
         const tEl = document.createElement("div");
         tEl.classList.add("record");
         tEl.innerHTML = element.ip + " , " + element.country_name + " , " + element.country_capital + " , " + element.state_prov
             + " , " + element.district + " , " + element.city + " , " + '<img class="flag" src="' + element.country_flag + '" />';
 
         elLocation.appendChild(tEl);
-
-        /*
-        const elClick = (map, tEl) => {
-
-            if (element.longitude !== undefined && element.latitude !== undefined) {
-                map.flyTo({ center: element.longitude, element.latitude});
-            }
-        };
-*/
         tEl.addEventListener("click", async () => {
 
             if (element.longitude !== undefined && element.latitude !== undefined) {
                 map.flyTo({ center: [element.longitude, element.latitude] });
-                var weatherUri = `/api/ipgeolocation/weather?lat=${element.longitude}&lon=${element.latitude}`;
-                console.log(weatherUri);
-                const weather = await fetch(weatherUri);
-                const weatherJson = await weather.json();
-                console.log(weatherJson);
             }
+
         });
 
     });
